97d9dfd1eb5044fe1adbf259ff885a687f27ebac
msyyn <36276167+msyyn@users.noreply.github.com>
2023-11-15 20:54:31 +0200

    particle effects

--- demo/src/main/java/com/example/demo/Plugin.java
+++ demo/src/main/java/com/example/demo/Plugin.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,6 +17,8 @@ import java.util.UUID;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+
+import org.bukkit.Location;
 
 public class Plugin extends JavaPlugin implements Listener {
 
@@ -42,32 +46,45 @@ public class Plugin extends JavaPlugin implements Listener {
     if (cmd.getName().equalsIgnoreCase("ignoreblock")) {
       // Ensure the command sender is a player
       if (!(sender instanceof Player)) {
-        sender.sendMessage("This command can only be used by a player.");
-        return true;
+          sender.sendMessage("This command can only be used by a player.");
+          return true;
       }
-
+  
       Player player = (Player) sender;
       PlayerInventory inventory = player.getInventory();
       Material itemInHand = inventory.getItemInMainHand().getType();
-
+  
       if (itemInHand == Material.AIR) {
           sender.sendMessage("You are not holding any item.");
           return true;
       }
-
+  
       PlayerPreferences prefs = playerPreferences.computeIfAbsent(player.getUniqueId(), k -> new PlayerPreferences());
       HashSet<Material> ignoredItems = prefs.getIgnoredItems();
-
+  
+      // Get the location of the player's main hand
+      Location handLocation = player.getEyeLocation();
+      handLocation.add(player.getEyeLocation().getDirection().multiply(1));
+  
       if (ignoredItems.contains(itemInHand)) {
           ignoredItems.remove(itemInHand);
           sender.sendMessage("You will now pick up " + itemInHand.name() + ".");
+  
+          // Add particle effect and sound for un-ignoring an item at the hand location
+          player.getWorld().spawnParticle(Particle.HEART, handLocation, 10); // Spawns heart particles at the hand location
+          player.playSound(handLocation, Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f); // Plays a level-up sound at the hand location
+  
       } else {
           ignoredItems.add(itemInHand);
           sender.sendMessage("You will no longer pick up " + itemInHand.name() + ".");
+  
+          // Add particle effect and sound for ignoring an item at the hand location
+          player.playSound(handLocation, Sound.BLOCK_NOTE_BLOCK_BASS, 1.0f, 1.0f); // Plays a note block bass sound at the hand location
       }
-
+  
       return true;
-    }
+   }
+  
 
     if (cmd.getName().equalsIgnoreCase("ignoreblocks")) {
       // Ensure the command sender is a player
